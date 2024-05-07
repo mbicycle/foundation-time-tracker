683c203e774830a12bee33101893d53f884cd176
HlebPinchukou <hleb.pinchukou@mbicycle.com>
2024-05-07 11:30:38 +0300

    Update ui kit

--- src/containers/application-bar/ButtonSet.tsx
+++ src/containers/application-bar/ButtonSet.tsx
@@ -1,30 +1,17 @@
 import { memo } from 'react';
-
-import FileUploadOutlinedIcon from '@mui/icons-material/FileUploadOutlined';
+import { Button } from '@mbicycle/foundation-ui-kit';
 
 import { ButtonText } from './constants';
-import { LoadingButtonStyled } from './styled';
 
 const PdfButtonSet = function (): JSX.Element | null {
   return (
     <>
-      <LoadingButtonStyled
-        startIcon={<FileUploadOutlinedIcon fontSize="medium" />}
-        variant="outlined"
-        color="secondary"
-        loadingPosition="start"
-        sx={{ color: (theme) => theme.palette.primary.main }}
-      >
+      <Button>
         {ButtonText.ExportDocx}
-      </LoadingButtonStyled>
-      <LoadingButtonStyled
-        startIcon={<FileUploadOutlinedIcon fontSize="medium" />}
-        variant="outlined"
-        color="secondary"
-        loadingPosition="start"
-      >
+      </Button>
+      <Button>
         {ButtonText.ExportPDF}
-      </LoadingButtonStyled>
+      </Button>
     </>
   );
 };
